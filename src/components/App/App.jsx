89d6410dbdc16a85d36b881c3efd2cc55ee3074b
Korotenko Alex <55750546+ToyEye@users.noreply.github.com>
2024-04-27 17:53:24 +0300

    sda

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import { lazy } from 'react';
 import Layout from '../Layout/Layout';
+
 const HomePage = lazy(() => import('../../pages/HomePage/HomePage'));
 const MoviesPage = lazy(() => import('../../pages/MoviesPage/MoviesPage'));
 const MovieDetailsPage = lazy(() => import('../../pages/MovieDetailsPage/MovieDetailsPage'));
@@ -15,7 +16,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/movies" element={<MoviesPage />} />
-        <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
+        <Route path="/movies/:movieId/" element={<MovieDetailsPage />}>
           <Route path="cast" element={<MovieCast />} />
           <Route path="reviews" element={<MovieReviews />} />
         </Route>
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,16 +1,12 @@
 import Navigation from '../Navigation/Navigation';
-import { Suspense } from "react";
+import { Suspense } from 'react';
 import css from './Layout.module.css';
 
-
 export default function Layout({ children }) {
-    
-    return (
-        <div className={css.container}>
-            <Navigation />
-            <Suspense fallback={<div>Please wait loading page...</div>}>
-                {children}
-            </Suspense>
-        </div>
-    )
+  return (
+    <div className={css.container}>
+      <Navigation />
+      <Suspense fallback={<div>Please wait loading page...</div>}>{children}</Suspense>
+    </div>
+  );
 }
