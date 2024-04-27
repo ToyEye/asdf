bed8fd6b5d4b6938f7fd9f9a55486e212bfacfe0
Korotenko Alex <55750546+ToyEye@users.noreply.github.com>
2024-04-27 19:20:08 +0300

    das

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "../src/components/App/App";
-import "modern-normalize";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+// import App from "./components/App/App";
+import 'modern-normalize';
+import './index.css';
+import App from './components/App/App';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
