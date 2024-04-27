188657748262da228f18bdaa94eb1e8a52cf81ae
Korotenko Alex <55750546+ToyEye@users.noreply.github.com>
2024-04-27 18:30:06 +0300

    das

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,10 +1,10 @@
-import css from './MovieList.module.css'
-import { NavLink } from "react-router-dom";
+import css from './MovieList.module.css';
+import { NavLink } from 'react-router-dom';
 
 export default function MovieList({ movies }) {
   return (
     <ul>
-      {movies.map((movie) => (
+      {movies.map(movie => (
         <li key={movie.id}>
           <NavLink to={`/movies/${movie.id}`}>{movie.original_title}</NavLink>
         </li>
